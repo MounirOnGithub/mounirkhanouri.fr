97105cca29346acc975f12a37e4c53bdb905aaff
MounirOnGithub <mkhanouri555@gmail.com>
2019-10-05 16:02:07 +0200

    added two presentation v-flex and integrated a carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>Home page</div>
+  <v-container fluid>
+    <v-layout row>
+      <v-flex xs12>
+        <HomeCarousel />
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex md6 xs12>
+        Presentation 1
+      </v-flex>
+      <v-flex md6 xs12>
+        Presentation 2
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import HomeCarousel from '@/components/HomeCarousel.vue';
+
 export default {
   name: 'Home',
+  components: {
+    HomeCarousel,
+  }
 }
 </script>
