fea6ddba2238615c43318c9b38cf235cc8115382
MounirOnGithub <mkhanouri555@gmail.com>
2019-10-05 15:17:21 +0200

    setting up fontawesome with vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
+import './plugins/fontawesome'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from "./router";
 import '@babel/polyfill'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+library.add(fab)
 
 Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
