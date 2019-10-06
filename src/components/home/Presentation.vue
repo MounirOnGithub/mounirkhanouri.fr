c50df84fefc02d7517df182237ecdea29ff57db3
MounirOnGithub <mkhanouri555@gmail.com>
2019-10-06 12:57:57 +0200

    child component for a section of the home page

---
+++ src/components/home/Presentation.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout column>
+    <v-flex>{{ icon }}</v-flex>
+    <v-flex text-xs-center>
+      <span class="title">
+        {{ title }}
+      </span>
+    </v-flex>
+    <v-flex text-xs-center>
+      <span>
+        {{ description }}
+      </span>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'Presentation',
+  props: {
+    icon: { type: String, default: '' },
+    title: { type: String, default: '' },
+    description: { type: String, default: '' },
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,19 @@
       </v-flex>
     </v-layout>
     <v-layout row>
-      <v-flex md6 xs12>
-        Presentation 1
+      <v-flex md6 xs12 text-xs-center>
+        <presentation 
+          :icon="firstPresentation.icon"
+          :title="firstPresentation.title"
+          :description="firstPresentation.description"
+        />
       </v-flex>
       <v-flex md6 xs12>
-        Presentation 2
+        <presentation 
+          :icon="secondPresentation.icon"
+          :title="secondPresentation.title"
+          :description="secondPresentation.description"
+        />
       </v-flex>
     </v-layout>
   </v-container>
@@ -18,11 +26,27 @@
 
 <script>
 import HomeCarousel from '@/components/HomeCarousel.vue';
+import Presentation from '@/components/home/Presentation.vue';
 
 export default {
   name: 'Home',
   components: {
     HomeCarousel,
-  }
+    presentation: Presentation,
+  },
+  data() {
+    return {
+      firstPresentation: {
+        icon: '',
+        title: this.$t('presentation.first.title'),
+        description: this.$t('presentation.first.description'),
+      },
+      secondPresentation: {
+        icon: '',
+        title: this.$t('presentation.second.title'),
+        description: this.$t('presentation.second.description'),
+      }
+    }
+  },
 }
 </script>
