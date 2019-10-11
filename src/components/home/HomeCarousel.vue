6f34c5f39e508199b2aa3d6decb4f4d4c91003fa
MounirOnGithub <mkhanouri555@gmail.com>
2019-10-11 14:09:13 +0200

    integrating a carousel with vue agile carousel component and added presentation components to home

---
+++ src/components/home/HomeCarousel.vue
@@ -0,0 +1,32 @@
+<template>
+  <agile :autoplay="true">
+    <template v-slot:default>
+      <v-img class="slide" src="https://via.placeholder.com/1500x500" />
+      <v-img class="slide" src="https://via.placeholder.com/1500x500" />
+    </template>
+
+    <template slot="prevButton">
+      <v-icon>mdi-chevron-left</v-icon>
+    </template>
+    <template slot="nextButton">
+      <v-icon>mdi-chevron-right</v-icon>
+    </template>
+  </agile>
+</template>
+
+<script>
+import { VueAgile } from "vue-agile";
+
+export default {
+  name: "HomeCarousel",
+  components: {
+    agile: VueAgile
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    Home
-  </div>
+  <v-layout row text-center>
+    <v-flex xs12>
+      <carousel />
+    </v-flex>
+    <v-flex md6 xs12 px-12>
+      <presentation
+        :title="$t('presentation.first.title')"
+        :description="$t('presentation.first.description')"
+      />
+    </v-flex>
+    <v-flex md6 xs12 px-12>
+      <presentation
+        :title="$t('presentation.second.title')"
+        :description="$t('presentation.second.description')"
+      />
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+import Presentation from "@/components/home/Presentation";
+import HomeCarousel from "@/components/home/HomeCarousel";
+
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-  },
+    presentation: Presentation,
+    carousel: HomeCarousel
+  }
 };
 </script>
