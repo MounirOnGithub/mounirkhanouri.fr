d997c479060795abcd487c55105cb34e037bb4ff
MounirOnGithub <mkhanouri555@gmail.com>
2019-10-12 02:35:41 +0200

    changed components for home carousel

--- src/components/home/HomeCarousel.vue
+++ src/components/home/HomeCarousel.vue
@@ -1,9 +1,23 @@
-<template></template>
+<template>
+  <carousel :autoplay="true" :autoplay-hover-pause="true" :per-page="1">
+    <slide>
+      <v-img src="https://via.placeholder.com/1500x500" />
+    </slide>
+    <slide>
+      <v-img src="https://via.placeholder.com/1500x500" />
+    </slide>
+  </carousel>
+</template>
 
 <script>
+import { Carousel, Slide } from "vue-carousel";
+
 export default {
   name: "HomeCarousel",
-  components: {},
+  components: {
+    Carousel,
+    Slide
+  },
   data() {
     return {};
   }
