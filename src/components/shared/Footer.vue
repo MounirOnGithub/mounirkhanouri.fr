5537684ab392c6bb4474105de1504dd52bb79504
MounirOnGithub <mkhanouri555@gmail.com>
2019-10-29 23:19:10 +0100

    footer integration of personal informations

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>this is a footer</div>
+  <v-layout row>
+    <v-flex xs4></v-flex>
+    <v-flex xs4></v-flex>
+    <v-flex xs4>
+      <v-layout column text-left py-2>
+        <v-flex py-1>
+          <span>{{ $t('footer.authorField') }} :</span>
+          {{ $t('footer.author.name') }}
+        </v-flex>
+        <v-flex py-1>
+          <span>{{ $t('footer.mailField') }} :</span>
+          {{ $t('footer.author.mail') }}
+        </v-flex>
+        <v-flex py-1>
+          <span>{{ $t('footer.locationField') }} :</span>
+          {{ $t('footer.author.location') }}
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
